aacb5426e3198bc6b67a8dae6a5653d086c1bc3d
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-09-28 18:25:48 +0300

    Lists Basic Exercise Solutions

---
+++ Lists Basics/04. Number Beggars.py
@@ -0,0 +1,41 @@
+fire_level = input()
+water = int(input())
+
+fire_level_list = fire_level.split("#")
+fire_level_list_2 = []
+fire_level_list_level = []
+fire_level_list_value = []
+valid_cells = []
+
+for i in fire_level_list:
+    fire_level_list_2.append(i.split(" = "))
+
+for i in fire_level_list_2:
+    fire_level_list_level.append(i[0])
+    fire_level_list_value.append(int(i[1]))
+
+for i in range(len(fire_level_list)):
+    if fire_level_list_level[i] == "High" \
+            and 81 <= fire_level_list_value[i] <= 125:
+        if water < fire_level_list_value[i]:
+            continue
+        water -= fire_level_list_value[i]
+        valid_cells.append(fire_level_list_value[i])
+    elif fire_level_list_level[i] == "Medium" \
+            and 51 <= fire_level_list_value[i] <= 80:
+        if water < fire_level_list_value[i]:
+            continue
+        water -= fire_level_list_value[i]
+        valid_cells.append(fire_level_list_value[i])
+    elif fire_level_list_level[i] == "Low" \
+            and 1 <= fire_level_list_value[i] <= 50:
+        if water < fire_level_list_value[i]:
+            continue
+        water -= fire_level_list_value[i]
+        valid_cells.append(fire_level_list_value[i])
+
+print("Cells:")
+for i in valid_cells:
+    print(f"- {i}")
+print(f"Effort: {sum(valid_cells) * 0.25:.2f}")
+print(f"Total Fire: {sum(valid_cells)}")
---
+++ Lists Basics/10. Bread Factory.py
@@ -0,0 +1,16 @@
+cards_str = input()
+cards = cards_str.split(" ")
+cards_shuffled = []
+cards_mid = len(cards) // 2
+n = int(input())
+
+for j in range(n):
+    for i in range(cards_mid):
+        cards_shuffled.append(cards[i])
+        cards_shuffled.append(cards[i + cards_mid])
+    cards = cards_shuffled
+    cards_shuffled = []
+
+print(cards)
+
+
