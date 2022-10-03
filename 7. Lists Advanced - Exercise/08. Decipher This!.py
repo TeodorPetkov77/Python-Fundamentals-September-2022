98fc6e343a5077b03d011a33c94c29011d79df96
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-10-03 19:51:06 +0300

    Update 08. Decipher This!.py

--- 7. Lists Advanced - Exercise/08. Decipher This!.py
+++ 7. Lists Advanced - Exercise/08. Decipher This!.py
@@ -1,15 +1,9 @@
 message = input().split(" ")
+
 for word in message:
-    code = ""
-    letters = ""
-    for char in word:
-        if char.isnumeric():
-            code += char
-        elif char.isalpha():
-            letters += char
-    if len(letters) == 1:
-        decoded = str(chr(int(code)) + letters[1:(len(letters) - 1)] + letters[0])
-    else:
-        decoded = str(chr(int(code)) + letters[len(letters) - 1] + \
-                  letters[1:(len(letters) - 1)] + letters[0])
-    print(decoded, end=" ")
+    code = "".join([i for i in word if i.isnumeric()])
+    letters = [i for i in word if i.isalpha()]
+    letters[0], letters[len(letters) - 1] = letters[len(letters) - 1], letters[0]
+    letters = "".join(letters)
+    print(f"{chr(int(code))}{letters}", end=" ")
+
