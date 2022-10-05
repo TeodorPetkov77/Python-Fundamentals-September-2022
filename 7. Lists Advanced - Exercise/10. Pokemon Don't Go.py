6e6d01da35adfd6e7c0ce4dc46743c5863f50608
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-10-05 16:57:53 +0300

    Update 10. Pokemon Don't Go.py

--- 7. Lists Advanced - Exercise/10. Pokemon Don't Go.py
+++ 7. Lists Advanced - Exercise/10. Pokemon Don't Go.py
@@ -1,22 +1,19 @@
 pokemons = list(map(int, input().split(" ")))
-list_of_removed_item = []
+total_points = 0
 
 while len(pokemons) > 0:
     index_to_remove = int(input())
     if index_to_remove < 0:
-        index_to_remove = 0
-        removed_item = pokemons.pop(0)
-        pokemons.insert(0, pokemons[len(pokemons) - 1])
+        removed_item = pokemons[0]
+        pokemons[0] = pokemons[-1]
     elif index_to_remove > len(pokemons) - 1:
-        index_to_remove = len(pokemons) - 1
-        removed_item = pokemons.pop(len(pokemons) - 1)
-        pokemons.append(removed_item)
+        removed_item = pokemons[-1]
+        pokemons[-1] = pokemons[0]
     else:
         removed_item = pokemons.pop(index_to_remove)
     pokemons = list(map(
         lambda x: x + removed_item if x <= removed_item
         else x - removed_item, pokemons
                 ))
-    list_of_removed_item.append(removed_item)
-    print(pokemons)
-print(sum(list_of_removed_item))
+    total_points += removed_item
+print(total_points)
