a4a59baf1f74682ea3b2e6ec59e3f0d94e05c8ca
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-10-12 20:11:00 +0300

    Update 04. Town.py

--- Exercises/9. Objects and Classes - Exericse/04. Town.py
+++ Exercises/9. Objects and Classes - Exericse/04. Town.py
@@ -16,3 +16,7 @@ class Town:
                f"| Longitude: {self.longitude}"
 
 
+town = Town("Sofia")
+town.set_latitude("42° 41\' 51.04\" N")
+town.set_longitude("23° 19\' 26.94\" E")
+print(town)
