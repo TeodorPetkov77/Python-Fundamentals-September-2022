1ffd066e506552d4655c5888b14cfdb9679c395f
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-10-13 11:31:49 +0300

    Update 06. Inventory.py

---
+++ Exercises/9. Objects and Classes - Exericse/06. Inventory.py
@@ -0,0 +1,25 @@
+class Inventory:
+    def __init__(self, capacity: int):
+        self.__capacity = capacity
+        self.items = []
+
+    def add_item(self, item: str):
+        if self.__capacity > len(self.items):
+            self.items.append(item)
+        else:
+            return "not enough room in the inventory"
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def __repr__(self):
+        return f"Items: {', '.join(self.items)}.\n" \
+               f"Capacity left: {self.__capacity - len(self.items)}"
+
+
+inventory = Inventory(2)
+inventory.add_item("potion")
+inventory.add_item("sword")
+print(inventory.add_item("bottle"))
+print(inventory.get_capacity())
+print(inventory)
