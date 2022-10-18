782e43de1fe9f0ef468252499a9cc88c2e259f46
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-10-18 16:00:45 +0300

    Update 12. SoftUni Exam Results.py

---
+++ Exercises/11. Dictionaries - Exercise/12. SoftUni Exam Results.py
@@ -0,0 +1,42 @@
+judge_exams = {}
+entry_list = []
+command = input()
+
+while command != "exam finished":
+    command = command.split("-")
+    username = command[0]
+    language = command[1]
+    if language != "banned":
+        entry_list.append(language)
+    if username in judge_exams and command[1] == "banned":
+        judge_exams[username][1]["banned"] = "yes"
+        command = input()
+        continue
+    points = int(command[2])
+    if username not in judge_exams:
+        judge_exams[username] = []
+        judge_exams[username].append({"max_points": 0})
+        judge_exams[username].append({"banned": "no"})
+    judge_exams[username].append({language: points})
+    if points > judge_exams[username][0]["max_points"]:
+        judge_exams[username][0]["max_points"] = points
+    command = input()
+
+print("Results:")
+for key in judge_exams.keys():
+    banned = False
+    for value in judge_exams[key]:
+        if value == {"banned": "yes"}:
+            banned = True
+            break
+    if banned is False:
+        print(f"{key} | {judge_exams[key][0]['max_points']}")
+
+print("Submissions:")
+for lan in dict.fromkeys(entry_list):
+    n = 0
+    for find in entry_list:
+        if find == lan:
+            n += 1
+    print(f"{lan} - {n}")
+
