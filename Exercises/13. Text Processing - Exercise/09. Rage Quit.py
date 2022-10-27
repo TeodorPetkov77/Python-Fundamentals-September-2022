da9a4b707b1fffb1f55bafc50780926cfbe1e3ca
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-10-27 17:22:04 +0300

    Update 09. Rage Quit.py

---
+++ Exercises/13. Text Processing - Exercise/09. Rage Quit.py
@@ -0,0 +1,37 @@
+def split_list(symbols: str):
+    temp_string = ""
+    non_num_list = []
+    num_list = []
+    unique = []
+    result = ""
+    for char in symbols:
+        if not char.isnumeric():
+            temp_string += char.upper()
+        else:
+            if temp_string:
+                non_num_list.append(temp_string)
+                temp_string = ""
+                continue
+    for char in symbols:
+        if char.isnumeric():
+            temp_string += char
+        else:
+            if temp_string:
+                num_list.append(int(temp_string))
+                temp_string = ""
+                continue
+    else:
+        num_list.append(int(temp_string))
+    for index in range(len(non_num_list)):
+        result += non_num_list[index] * num_list[index]
+
+    for s in symbols:
+        if not s.isnumeric() and s.upper() not in unique:
+            unique.append(s.upper())
+
+    print(f"Unique symbols used: {len(unique)}")
+    print(result)
+
+
+split_list(input())
+
