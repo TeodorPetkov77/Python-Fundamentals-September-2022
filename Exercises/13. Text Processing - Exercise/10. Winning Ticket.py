d4c0a79a41aeb047358906fc03e98bfaf1554962
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-10-28 19:31:48 +0300

    Update 10. Winning Ticket.py

--- Exercises/13. Text Processing - Exercise/10. Winning Ticket.py
+++ Exercises/13. Text Processing - Exercise/10. Winning Ticket.py
@@ -60,3 +60,38 @@ for ticket in tickets:
         print(f'ticket "{ticket}" - {min(uninterrupted_values)}{winning_char}')
     else:
         print(f'ticket "{ticket}" - no match')
+
+
+# https://judge.softuni.org/Contests/Compete/Index/1740#9
+
+# 10.	*Winning Ticket
+# The lottery is exciting. However, checking a million tickets for winnings only by hand is not. So, you are given the task of creating a program that automatically checks if a ticket is a winner.
+# You are given a collection of tickets separated by commas and spaces (one or many). You need to check each ticket to see if it has a winning combination of symbols:
+# •	A valid ticket has exactly 20 characters.
+# •	A winning ticket is a valid one, containing one of the symbols '@', '#', '$' or '^' uninterruptedly repeated at least 6 times in both halves of the tickets.
+# •	In order to win a Jackpot, the ticket should contain the same winning symbol 10 times on both sides
+# An example of a valid winning ticket:
+# "Cash$$$$$$Ca$$$$$$sh"
+# An example of a Jackpot winning valid ticket:
+# "$$$$$$$$$$$$$$$$$$$$"
+# Input
+# The input will be read from the console. The input consists of a single line containing all tickets separated by commas and one or more white spaces in the format:
+# •	"{ticket}, {ticket}, … {ticket}"
+# Output
+# Print the result for every ticket in the order of their appearance, each on a separate line in the format:
+# •	If the ticket is invalid: "invalid ticket"
+# •	If the ticket is valid, but it is not winning: "ticket "{ticket}" - no match"
+# •	If the ticket is valid and winning, but not a Jackpot:
+# "ticket "{ticket}" - {uninterrupted_match_length}{match_symbol}"
+# •	It the ticket hits the Jackpot:
+# "ticket "{ticket}" - {uninterrupted_match_length}{match_symbol} Jackpot!"
+# Constrains
+# •	Number of tickets will be in range [0 … 100]
+# Examples
+# Input	Output
+# Cash$$$$$$Ca$$$$$$sh	ticket "Cash$$$$$$Ca$$$$$$sh" - 6$
+# $$$$$$$$$$$$$$$$$$$$, aabb  , th@@@@@@eemo@@@@@@ey	ticket "$$$$$$$$$$$$$$$$$$$$" - 10$ Jackpot!
+# invalid ticket
+# ticket "th@@@@@@eemo@@@@@@ey" - 6@
+# validticketnomatch:(, Cas$$$$$$$Ca$$$$$$s$	ticket "validticketnomatch:(" - no match
+# ticket "Cas$$$$$$$Ca$$$$$$s$" - 6$
