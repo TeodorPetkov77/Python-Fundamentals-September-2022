0d30f5bb92a68b14cc42aca47ffa5b74d90e24c9
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-11-04 13:47:33 +0200

    Update 06. Extract the Links.py

--- Exercises/15. Regular Expressions - Exercise/06. Extract the Links.py
+++ Exercises/15. Regular Expressions - Exercise/06. Extract the Links.py
@@ -3,7 +3,7 @@ import re
 pattern = r'((www[\.]){1})([a-zA-Z0-9\-]+)([\.]{1}[a-z]+)([\.]{1}[a-z]+)*'
 text = input()
 
-while len(text):
+while text:
     links = re.finditer(pattern, text)
     for i in links:
         print(i.group())
