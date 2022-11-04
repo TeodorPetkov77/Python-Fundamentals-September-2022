210016a8802ffae2ff83c17e58fa0d5029697bd6
RebelShark <107725014+RebelShark@users.noreply.github.com>
2022-11-04 17:27:04 +0200

    x

--- More exercises/Lists Advanced - More Exercises/01. Social Distribution.py
+++ More exercises/Lists Advanced - More Exercises/01. Social Distribution.py
@@ -14,3 +14,16 @@ else:
     print(population)
 
 # https://judge.softuni.org/Contests/Practice/Index/1732#0
+
+# 1.	Social Distribution
+# A core idea of several left-wing ideologies is that the wealthiest should support the poorest, no matter what, and that is exactly what you are called to do for this problem.
+# On the first line, you will be given the population (numbers separated by comma and space ", "). On the second line, you will be given the minimum wealth. You should distribute the wealth so that no part of the population has less than the minimum wealth. To do that, you should always take wealth from the wealthiest part of the population.
+# There will be cases where the distribution will not be possible. In that case, print: "No equal distribution possible".
+# Example
+# Input	Output
+# 2, 3, 5, 15, 75
+# 5	[5, 5, 5, 15, 70]
+# 2, 3, 5, 15, 75
+# 20	[20, 20, 20, 20, 20]
+# 2, 3, 5, 45, 45
+# 30	No equal distribution possible
--- More exercises/Lists Advanced - More Exercises/02. Take_Skip Rope.py
+++ More exercises/Lists Advanced - More Exercises/02. Take_Skip Rope.py
@@ -14,3 +14,29 @@ for i in range(0, len(take_list)):
 print(result_string)
 
 # https://judge.softuni.org/Contests/Practice/Index/1732#1
+
+# 2.	Take/Skip Rope
+# Write a program, which reads a string and skips through it, extracting a hidden message. The algorithm you should implement is as follows:
+# Let us take the string "skipTest_String044160" as an example.
+# Take every digit from the string and transfer it somewhere. After this operation, you should have two lists of items - a numbers list and a non-numbers list:
+# •	Numbers' list: [0, 4, 4, 1, 6, 0]
+# •	Non-numbers: [s, k, i, p, T, e, s, t, _, S, t, r, i, n, g]
+# After that, take every digit in the numbers list and split it up into a take list and a skip list. In the take list, you should keep all digits at an even index. In the skip list, you should keep all digits at an odd index.
+# •	Numbers' list: [0, 4, 4, 1, 6, 0]
+# •	Take list: [0, 4, 6]
+# •	Skip list: [4, 1, 0]
+# Afterward, iterate over both lists:
+# •	First, take m characters from the non-numbers list and store it in a result string
+# •	Then, skip n characters from the non-numbers list
+# Note that the skipped characters are summed up as they go. The process would look like this:
+# 1.	Current string: "skipTest_String". Take 0 characters and skip 4 characters:
+# •	Taken string: ""
+# •	Skipped string: "skip"
+# 2.	The remaining string looks like this: "Test_String". Take 4 characters and skip 1 character:
+# •	Taken string: "Test"
+# •	Skipped string: "_"
+# 3.	The string looks like this: "String". Take 6 characters and skip 0 characters:
+# •	Taken string: "String"
+# •	Skipped string: ""
+# 4.	The final string is "TestString".
+# After that, print the final string on the console.
