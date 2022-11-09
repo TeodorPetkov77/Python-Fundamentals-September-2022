f5808678ce0906f02f0c81f3da3f8506be66ed0d
RebelShark <tpetkov93@gmail.com>
2022-11-09 16:11:30 +0200

    Update 3_longer_line.py

--- More exercises/functions_more_exercises/3_longer_line.py
+++ More exercises/functions_more_exercises/3_longer_line.py
@@ -16,7 +16,8 @@ def center_point_not(x1, y1, x2, y2):
 
 
 def longest_line(x1_1, y1_1, x2_1, y2_1, x1_2, y1_2, x2_2, y2_2):
-    if abs(x1_1 - x2_1) + abs(y1_1 - y2_1) >= abs(x1_2 - x2_2) + abs(y1_2 - y2_2):
+    if math.sqrt((abs(x1_1 - x2_1)) ** 2 + (abs(y1_1 - y2_1)) ** 2 >= \
+                 (abs(x1_2 - x2_2)) ** 2 + (abs(y1_2 - y2_2)) ** 2):
         print(f"{center_point(x1_1, y1_1, x2_1, y2_1)}"
               f"{center_point_not(x1_1, y1_1, x2_1, y2_1)}")
     else:
@@ -36,6 +37,5 @@ y2_input_2 = float(input())
 
 longest_line(x1_input_1, y1_input_1, x2_input_1, y2_input_1,
              x1_input_2, y1_input_2, x2_input_2, y2_input_2)
-
 
 ### WORK IN PROGRESS ### 60/100 ###
