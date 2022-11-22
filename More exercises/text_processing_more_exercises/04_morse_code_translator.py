b751cb999f4d23690c8fa459d5c3322c62c587b2
RebelShark <tpetkov93@gmail.com>
2022-11-22 14:59:26 +0200

    Update 04_morse_code_translator.py

--- More exercises/text_processing_more_exercises/04_morse_code_translator.py
+++ More exercises/text_processing_more_exercises/04_morse_code_translator.py
@@ -60,3 +60,13 @@ def morse_translate(morse_list_input: list):
 
 morse_list = [item.split() for item in input().split(' | ')]
 morse_translate(morse_list)
+
+# https://judge.softuni.org/Contests/Practice/Index/1741#3
+
+# 4.	Morse Code Translator
+# Write a program that translates messages from Morse code to English (capital letters). Use this page to help you (without the numbers). The words will be separated by a space (' '). Each word is separated by a ' | '.
+# Print the found words on one line, separated by a space.
+# Example
+# Input	Output
+# .. | -- .- -.. . |  -.-- --- ..- | .-- .-. .. - . | .- | .-.. --- -. --. | -.-. --- -.. .	I MADE YOU WRITE A LONG CODE
+# .. | .... --- .--. . | -.-- --- ..- | .- .-. . | -. --- - | -- .- -..	I HOPE YOU ARE NOT MAD
