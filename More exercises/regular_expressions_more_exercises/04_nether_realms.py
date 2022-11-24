6ea7a09195ee69702d7cfa5557ef453fb511552d
RebelShark <tpetkov93@gmail.com>
2022-11-24 19:03:21 +0200

    x

--- More exercises/regular_expressions_more_exercises/04_nether_realms.py
+++ More exercises/regular_expressions_more_exercises/04_nether_realms.py
@@ -1,10 +1,25 @@
 import re
 
+demon_names = [x.strip() for x in input().split(',')]
+demon_dictionary = {}
+
 health_pattern = re.compile('[^\d\+\-\*\/\.]')
 damage_pattern = re.compile('[\-]?[0-9]+[\.]?[0-9]*')
 operators_pattern = re.compile('[\*\/]')
 
-demon_names = 'M3ph-0.5s-0.5t0.0**'
-demon_health = sum([ord(x) for x in re.findall(health_pattern, demon_names)])
-demon_damage = re.findall(damage_pattern, demon_names)
-print(re.findall(operators_pattern, demon_names))
+for demon_name in demon_names:
+
+    demon_health = sum([ord(x) for x in re.findall(health_pattern, demon_name)])
+    demon_damage = sum([float(x) for x in re.findall(damage_pattern, demon_name)])
+    operators = re.findall(operators_pattern, demon_name)
+
+    for operator in operators:
+        if operator == "*":
+            demon_damage *= 2
+        elif operator == "/":
+            demon_damage /= 2
+    demon_dictionary[demon_name] = \
+        f'{demon_health} health, {demon_damage:.2f} damage'
+
+for key, value in sorted(demon_dictionary.items()):
+    print(f'{key} - {value}')
