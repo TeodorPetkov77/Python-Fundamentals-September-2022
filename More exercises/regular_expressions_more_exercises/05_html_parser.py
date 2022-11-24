d7f012bff39327cde6abace0a94a14b35a67cf10
RebelShark <tpetkov93@gmail.com>
2022-11-24 23:07:42 +0200

    Update 05_html_parser.py

--- More exercises/regular_expressions_more_exercises/05_html_parser.py
+++ More exercises/regular_expressions_more_exercises/05_html_parser.py
@@ -12,9 +12,7 @@ if re.search(pattern_title, modded_text).group():
 
 modded_text = re.sub(r'<title>(\w+\s*)+</title>', '', modded_text)
 
-tags_to_remove = [x.group() for x in re.finditer(remove_tags_pattern, modded_text)]
-
-for tag in tags_to_remove:
+for tag in [x.group() for x in re.finditer(remove_tags_pattern, modded_text)]:
     modded_text = re.sub(tag, '', modded_text)
 
 print(f'Content: {modded_text}')
