ee6bb60ec78ff253c209aa3af9a336297b429c89
RebelShark <tpetkov93@gmail.com>
2022-11-30 16:51:50 +0200

    Update 02_mirror_words.py

--- Exams/Final Exams/03_programming_fundamentals_final_exam_retake/02_mirror_words.py
+++ Exams/Final Exams/03_programming_fundamentals_final_exam_retake/02_mirror_words.py
@@ -5,7 +5,7 @@ pattern = re.compile(r'(\@|\#)(?P<word_1>[A-Za-z]{3,})\1{2}(?P<word_2>[A-Za-z]{3
 mirror_words = []
 valid = 0
 
-if re.findall(pattern, text_input):
+if re.search(pattern, text_input):
     for valid, item in enumerate(re.finditer(pattern, text_input)):
         if item.group('word_1') == item.group('word_2')[::-1]:
             mirror_words.append(f'{item.group("word_1")} <=> {item.group("word_2")}')
